5dc56ec3e756ac0a8b23c7ce70f6617969f350fd
Reed Martz <reed@reedmartz.com>
2017-12-31 10:51:49 -0500

    Mark ingredients as optional and show quantity

--- django/recipes/serializers.py
+++ django/recipes/serializers.py
@@ -4,6 +4,15 @@ from recipes.models import Recipe, Ingredient
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = serializers.SerializerMethodField()
+
+    def get_ingredients(self, obj):
+        return [{
+            'id': ri.ingredient.id,
+            'optional': ri.optional,
+            'quantity': ri.quantity
+        } for ri in obj.recipeingredient_set.all()]
+
     class Meta:
         model = Recipe
         fields = ('id', 'label', 'ingredients')
