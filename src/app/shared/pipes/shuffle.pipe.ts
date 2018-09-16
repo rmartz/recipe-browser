f0a0ec811bc02361c0dc79e1ab0cc0ad39c2aaee
Reed Martz <reed@reedmartz.com>
2018-09-16 09:21:11 -0400

    Use Fisher-Yates algorithm for fair shuffle

--- src/app/shared/pipes/shuffle.pipe.ts
+++ src/app/shared/pipes/shuffle.pipe.ts
@@ -6,6 +6,23 @@ import { PipeTransform, Pipe } from '@angular/core';
 })
 export class ShufflePipe implements PipeTransform {
   transform(items: any[]): any[] {
-    return items.sort(() => Math.random() - 0.5);
-  }
+      // Implementation of Fisher Yates shuffle algorithm, adapted from Mike Bostock
+      // https://bost.ocks.org/mike/shuffle/
+      let m = items.length;
+
+      // While there remain elements to shuffle…
+      while (m) {
+
+        // Pick a remaining element…
+        const i = Math.floor(Math.random() * m);
+        m -= 1;
+
+        // And swap it with the current element.
+        const t = items[m];
+        items[m] = items[i];
+        items[i] = t;
+      }
+
+      return items;
+    }
 }
