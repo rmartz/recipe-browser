569cb157eb4785527ca21c3dbba2c5b6032e4ff6
Reed Martz <reed@reedmartz.com>
2018-09-16 10:03:18 -0400

    Simplify ingredient sidebar

--- src/app/ingredients-filter/ingredients-filter.component.ts
+++ src/app/ingredients-filter/ingredients-filter.component.ts
@@ -9,24 +9,12 @@ import { IngredientWeight } from '../shared/models/ingredient.model';
 export class IngredientsFilterComponent {
   constructor(public suggestions: Suggestions) { }
 
-  public isBlacklisted(suggestion: IngredientWeight) {
-    return suggestion.ingredient.blacklisted;
-  }
-
-  public isFavorited(suggestion: IngredientWeight) {
-    return suggestion.ingredient.favorited;
-  }
-
-  public isCommon(suggestion: IngredientWeight) {
-    return (!suggestion.ingredient.favorited
-            && (suggestion.weight === 0)
-            && (suggestion.occurrences > 0));
+  public isRated(suggestion: IngredientWeight) {
+    return suggestion.ingredient.blacklisted || suggestion.ingredient.favorited;
   }
 
   public isSuggestion(suggestion: IngredientWeight) {
     return (!suggestion.ingredient.favorited
-            && suggestion.weight > 0
-            && suggestion.additions > 0);
+            && (suggestion.occurrences > 0));
   }
-
 }
--- src/app/shared/pipes/sort-by.pipe.ts
+++ src/app/shared/pipes/sort-by.pipe.ts
@@ -5,7 +5,26 @@ import { PipeTransform, Pipe } from '@angular/core';
     pure: false
 })
 export class SortByPipe implements PipeTransform {
-  transform(items: any[], key: string): any[] {
-    return items.sort((a, b) => b[key] - a[key]);
+  private get_sort(keys: string[]) {
+    function sort(a, b) {
+      for (let key of keys) {
+        let direction = 1;
+        if (key.charAt(0) === '-') {
+          direction = -1;
+          key = key.substr(1);
+        }
+        const diff = a[key] - b[key];
+        if (diff !== 0) {
+          return direction * diff;
+        }
+      }
+      return 0;
+    }
+    return sort;
+  }
+
+  transform(items: any[], keys: string[]): any[] {
+    const sort = this.get_sort(keys);
+    return items.sort(sort);
   }
 }
