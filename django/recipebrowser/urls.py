8938387f419a145fd874dfb6bcd04b981be9e3c1
Reed Martz <reed@reedmartz.com>
2018-10-28 11:15:56 -0400

    Remove Django project source files

--- django/recipebrowser/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
-    url(r'^api/', include('recipes.urls')),
-    url(r'^admin/', admin.site.urls),
-]
